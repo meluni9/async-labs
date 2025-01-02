d08062a651fc6bf1e8c3926d864be75ea9ea6a1d
ani163 <melnika243@gmail.com>
2025-01-02 20:44:57 +0200

    Optimize isPrime

--- additional-task/task.js
+++ additional-task/task.js
@@ -1,6 +1,6 @@
 const isPrime = (n) => {
     if (n < 2) return false;
-    for (let i = 2; i <= n - 1; i++) {
+    for (let i = 2; i <= Math.sqrt(n); i++) {
         if (n % i === 0) return false;
     }
     return true;
